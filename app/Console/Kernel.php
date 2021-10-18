4895ffea0c929613715f24f9acc87e0da3d06257
nirattasai <nirattasai@gmail.com>
2021-10-19 03:21:49 +0700

    update schedule task

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -45,7 +45,7 @@ class Kernel extends ConsoleKernel
             foreach($logs as $log){
                 $login_time = strtotime($log->login_time);
                 $late_time = strtotime(env('LATE_TIME'));
-                if(($log->login_time == "00:00:01" or $log->logout_time == "00:00:01") and $log->is_leave == 0){
+                if(($log->login_time == "00:00:01") and $log->is_leave == 0){
                     DB::update("update logs set is_absent = 1 where id = ?",[$log->id]);
                 }
                 else if(($login_time >= $late_time) and $log->is_leave == 0){
